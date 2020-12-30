f30b596217210c9233083762b7f05a67cc697c66
bubaley <bubaley.fu@gmail.com>
2020-12-30 04:04:59 +0200

    edit auth

--- auth.js
+++ auth.js
@@ -1,147 +1,146 @@
-module.exports = (function () {
-    const axios = window && window.axios ? window.axios : require('./axios')
-    const self = {}
-    self.user = null
-    self.actions = []
+const axios = window && window.axios ? window.axios : require('./axios')
 
-    self.hasRight = (data) => {
-        if (!self.user)
-            return false
-        if (self.user && self.user.is_superuser)
-            return true
-        return !!self.actions.find(action => action.group_name === data[0] && action.name === data[1])
-    }
+const self = {}
+self.user = null
+self.actions = []
 
-    self.hasRights = (data) => {
-        for (let el of data) {
-            if (!self.hasRight(el))
-                return false
-        }
-        return true
-    }
-
-    self.hasOneOfRights = (data) => {
-        for (let el of data) {
-            if (self.hasRight(el))
-                return true
-        }
+self.hasRight = (data) => {
+    if (!self.user)
         return false
-    }
+    if (self.user && self.user.is_superuser)
+        return true
+    return !!self.actions.find(action => action.group_name === data[0] && action.name === data[1])
+}
 
-    self.isSuperuser = () => {
-        return self.user && self.user.is_superuser
+self.hasRights = (data) => {
+    for (let el of data) {
+        if (!self.hasRight(el))
+            return false
     }
+    return true
+}
 
-    self.me = (needRefresh = true) => {
-        return new Promise((resolve, reject) => {
-            let access = localStorage.getItem('access')
-            if (access && access !== 'undefined') {
-                axios.defaults.headers.common = {Authorization: 'Bearer ' + access}
-                axios.get('/users/me/')
-                    .then(response => {
-                        self.user = response.data
-                        resolve(response.data)
-                    })
-                    .catch((error) => {
-                        if (needRefresh) {
-                            self.refresh().then(() => {
-                                self.me(false).then(data => {
-                                    resolve(data)
-                                }).catch(() => {
-                                    reject()
-                                })
+self.hasOneOfRights = (data) => {
+    for (let el of data) {
+        if (self.hasRight(el))
+            return true
+    }
+    return false
+}
+
+self.isSuperuser = () => {
+    return self.user && self.user.is_superuser
+}
+
+self.me = (needRefresh = true) => {
+    return new Promise((resolve, reject) => {
+        let access = localStorage.getItem('access')
+        if (access && access !== 'undefined') {
+            axios.defaults.headers.common = {Authorization: 'Bearer ' + access}
+            axios.get('/users/me/')
+                .then(response => {
+                    self.user = response.data
+                    resolve(response.data)
+                })
+                .catch((error) => {
+                    if (needRefresh) {
+                        self.refresh().then(() => {
+                            self.me(false).then(data => {
+                                resolve(data)
                             }).catch(() => {
                                 reject()
                             })
-                        } else {
-                            reject()
-                        }
-                    })
-            } else {
-                if (needRefresh)
-                    self.refresh().then(() => {
-                        self.me(false).then(data => {
-                            resolve(data)
                         }).catch(() => {
                             reject()
                         })
+                    } else {
+                        reject()
+                    }
+                })
+        } else {
+            if (needRefresh)
+                self.refresh().then(() => {
+                    self.me(false).then(data => {
+                        resolve(data)
                     }).catch(() => {
-                        self.logout().then(() => {
-                            reject()
-                        })
+                        reject()
                     })
-                else {
+                }).catch(() => {
                     self.logout().then(() => {
                         reject()
                     })
-                }
+                })
+            else {
+                self.logout().then(() => {
+                    reject()
+                })
             }
+        }
+    })
+}
+
+self.login = (data) => {
+    const url = '/token/';
+
+    return new Promise((resolve, reject) => {
+        axios.post(url, data).then(response => {
+            localStorage.setItem('access', response.data.access)
+            localStorage.setItem('refresh', response.data.refresh)
+            self.me(false).then((user) => {
+                resolve({
+                    user: user,
+                    tokens: response.data
+                })
+            })
+        }).catch(error => {
+            reject(error.response)
         })
-    }
+    })
+}
 
-    self.login = (data) => {
-        const url = '/token/';
+self.register = (data) => {
+    return new Promise((resolve, reject) => {
+        axios.post('/users/', data).then(response => {
+            self.user = response.data
+            self.login(data).then(tokens => {
+                resolve({
+                    user: response.data,
+                    tokens: tokens
+                })
+            })
+        }).catch(error => {
+            reject(error.response)
+        })
+    })
+}
 
-        return new Promise((resolve, reject) => {
-            axios.post(url, data).then(response => {
+self.refresh = () => {
+    let refresh = localStorage.getItem('refresh')
+    return new Promise((resolve, reject) => {
+        if (refresh && refresh !== 'undefined') {
+            axios.post('/token/refresh/', {
+                refresh: refresh
+            }).then(response => {
                 localStorage.setItem('access', response.data.access)
                 localStorage.setItem('refresh', response.data.refresh)
-                self.me(false).then((user) => {
-                    resolve({
-                        user: user,
-                        tokens: response.data
-                    })
-                })
+                resolve()
             }).catch(error => {
                 reject(error.response)
             })
-        })
-    }
+        } else {
+            reject()
+        }
+    })
+}
 
-    self.register = (data) => {
-        return new Promise((resolve, reject) => {
-            axios.post('/users/', data).then(response => {
-                self.user = response.data
-                self.login(data).then(tokens => {
-                    resolve({
-                        user: response.data,
-                        tokens: tokens
-                    })
-                })
-            }).catch(error => {
-                reject(error.response)
-            })
-        })
-    }
+self.logout = () => {
+    return new Promise((resolve, reject) => {
+        axios.defaults.headers.common = {Authorization: null}
+        localStorage.removeItem('access')
+        localStorage.removeItem('refresh')
+        self.user = null
+        resolve()
+    })
+}
 
-    self.refresh = () => {
-        let refresh = localStorage.getItem('refresh')
-        return new Promise((resolve, reject) => {
-            if (refresh && refresh !== 'undefined') {
-                axios.post('/token/refresh/', {
-                    refresh: refresh
-                }).then(response => {
-                    localStorage.setItem('access', response.data.access)
-                    localStorage.setItem('refresh', response.data.refresh)
-                    resolve()
-                }).catch(error => {
-                    reject(error.response)
-                })
-            } else {
-                reject()
-            }
-        })
-    }
-
-    self.logout = () => {
-        return new Promise((resolve, reject) => {
-            axios.defaults.headers.common = {Authorization: null}
-            localStorage.removeItem('access')
-            localStorage.removeItem('refresh')
-            self.user = null
-            resolve()
-        })
-    }
-
-    return self
-})()
+module.exports = self
